e528bd5de1d987e74c2e9a121541ffb490919fc0
diegoc-pimentel <diego05.pimentel@gmail.com>
2025-07-03 15:03:35 -0300

    second commit

--- script.js
+++ script.js
@@ -5,8 +5,10 @@ var menu = document.getElementById('menu');
 menuBtn.onclick = function(){
     if(sideNav.style.right == "-250px"){
         sideNav.style.right = "0";
+        menu.src= "images/close.png";
     }
     else{
         sideNav.style.right = "-250px";
+        menu.src= "images/menu.png";
     }
 }
